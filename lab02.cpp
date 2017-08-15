259dc56c0eb63a23c05b0798e8a8d444924f391f
Misael Batista Damasceno <misael19972008@hotmail.com>
2017-08-15 20:10:12 -0300

    lab02-q1

--- lab02.cpp
+++ lab02.cpp
@@ -3,26 +3,42 @@
 
 using namespace std;
 
-void concat(std::vector<int> vtr1 ;std::vector<int> vtr2;){
+void concat(vector<int> &vtr1,vector<int> &vtr2, vector<int> &vtr3){
+	
+
 	for (unsigned int i = 0;i< vtr1.size();i++){
-		vtr3+=vtr1[i];
-	}
+		vtr3.push_back(vtr1[i]);
+	}	
 
 	for (unsigned int i = 0;i< vtr2.size();i++){
-		vtr3+=vtr2[i];
+		vtr3.push_back(vtr2[i]);
 	}
 	
 
 
 
 }
-
 int main(){
-	vector <int> vtr1;
-	vector <int> vtr2;
-	vector <int> vtr3;
-	cin>> vtr1 >>endl;
-	cin>> vtr2 >>endl;
+	vector<int> vtr1;	
+	vector<int> vtr2;
+	vector<int> vtr3;
+	int aux;
+	for (unsigned int i = 0; i < 2; ++i){
+		cin>>aux;
+		vtr1.push_back(aux);  	
+	}
+	
+	for (unsigned int i = 0; i < 2; ++i){
+		cin>>aux;
+		vtr2.push_back(aux);  	
+  	
+	}
+	
 	concat(vtr1,vtr2,vtr3);
-	cout << vtr3 << endl;
+  	
+  	for (int i = 0; i < vtr3.size(); ++i){
+  		cout<<vtr3[i]; 	
+  	}
+  	cout<<endl;
+  	return 0;
 }
